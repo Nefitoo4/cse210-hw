29dd74856284b5de784a235e1f4bb2c3ef1f5015
Nefitoo4 <nefizaldana@gmail.com>
2023-09-24 15:41:52 -0600

    learning02 last update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
@@ -24,18 +24,31 @@ class Program
         
         myResume.Display();
     }
-    public class Job
+}
+public class Job
     {
         public string _jobtitle;
         public string _company;
         public int _startYear;
         public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}");
+        }
     }
 
     public class Resume
     {
         public string _name;
-        List<Job> _jobs = new List<Job>();
-    }
+        public List<Job> _jobs = new List<Job>();
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs: ");
 
-}
+            foreach (Job job in _jobs){
+                job.Display();
+            }
+        }
+    }
