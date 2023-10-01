20f5900a831dd71f8885d7ec9cd01a9fd8fa166d
Nefitoo4 <nefizaldana@gmail.com>
2023-09-30 20:03:34 -0600

    Journal assignment

---
+++ prepare/Learning02/JournalW03.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        
+    }
+}
