674199bf3dbb6d3f87feb2eafce10f3f45d94277
Nefitoo4 <nefizaldana@gmail.com>
2023-11-15 09:52:53 -0600

    classes separated

--- csharp-prep/Prep1/person.cs
+++ csharp-prep/Prep1/person.cs
@@ -3,13 +3,13 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Classes
 {
-        public class Person
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void Introduce()
         {
-            public string FirstName;
-            public string LastName;
-            public void Introduce()
-            {
-                Console.WriteLine("My name is " + FirstName + " " + LastName);
-            }
-        }    
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }    
 }
