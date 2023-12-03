2fbb9f5b4247b3685d106c21d527f8b5a8c5f476
Nefitoo4 <nefizaldana@gmail.com>
2023-12-02 22:07:24 -0600

    YouTube Videos_01

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+
+        // Create videos and add comments
+        Video video1 = new Video
+        {
+            Title = "C# Basics",
+            Author = "CodingExpert",
+            LengthInSeconds = 300
+        };
+
+        video1.AddComment("User1", "Great video!");
+        video1.AddComment("User2", "Very helpful content.");
+        video1.AddComment("User3", "I have a question.");
+
+        Video video2 = new Video
+        {
+            Title = "Object-Oriented Programming",
+            Author = "CodeMaster",
+            LengthInSeconds = 450
+        };
+
+        video2.AddComment("User4", "Awesome explanation!");
+        video2.AddComment("User5", "Clear and concise.");
+        video2.AddComment("User6", "Looking forward to more tutorials.");
+
+        // Add videos to the list
+        videos.Add(video1);
+        videos.Add(video2);
+
+        // Display information for each video
+        foreach (var video in videos)
+        {
+
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Foundation1
+{
+    internal class Video
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int LengthInSeconds { get; set; }
+        private List<Comment> comments = new List<Comment>();
+
+        public void AddComment(string commenterName, string commentText)
+        {
+            Comment comment = new Comment
+            {
+                CommenterName = commenterName,
+                CommentText = commentText
+            };
+            comments.Add(comment);
+        }
+
+        public int GetNumberOfComments()
+        {
+            return comments.Count;
+        }
+
+        public void DisplayVideoInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Length: {LengthInSeconds} seconds");
+            Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+
+            Console.WriteLine("Comments:");
+            foreach (var comment in comments)
+            {
+                Console.WriteLine($"  {comment.CommenterName}: {comment.CommentText}");
+            }
+
+            Console.WriteLine("\n-----------------------------\n");
+        }
+    }
+}
